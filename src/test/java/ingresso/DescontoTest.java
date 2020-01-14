4c992a45240d00a753719161af1bae9ec501996b
Eduardo <eduardoa8142@gmail.com>
2020-01-13 22:36:47 -0300

    Implementação da interface e das classes de desconto

---
+++ src/test/java/ingresso/DescontoTest.java
@@ -0,0 +1,66 @@
+package ingresso;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import br.com.caelum.ingresso.model.Filme;
+import br.com.caelum.ingresso.model.Ingresso;
+import br.com.caelum.ingresso.model.Sala;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.descontos.DescontoParaBancos;
+import br.com.caelum.ingresso.model.descontos.DescontoParaEstudantes;
+import br.com.caelum.ingresso.model.descontos.SemDesconto;
+import junit.framework.Assert;
+
+public class DescontoTest {
+	
+	Sala sala;
+	Filme filme;
+	Sessao sessao;
+	
+	@Before
+	public void requisitosParaOsTestesRodarem() {
+		
+		sala = new Sala("Eldorado - IMAX", new BigDecimal("20.5"));
+		filme = new Filme("Rogue One", Duration.ofMinutes(120), "SCI-FI", new BigDecimal("12"));
+		sessao = new Sessao(LocalTime.parse("10:00:00"), filme, sala);
+		
+	}
+	
+	@Test
+	public void naoDeveConcederDescontoParaIngressoNormal() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new SemDesconto());
+		
+		BigDecimal precoEsperado = new BigDecimal("32.50");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		
+	}
+	
+	@Test
+	public void deveConcederDescontoDe30PorcentoParaIngressoDeEstudante() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaBancos());
+		
+		BigDecimal precoEsperado = new BigDecimal("22.75");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		
+	}
+	
+	@Test
+	public void deveConcederDescontoDe50PorcentoParaIngressoDeEstudante() {
+		
+		Ingresso ingresso = new Ingresso(sessao, new DescontoParaEstudantes());
+		
+		BigDecimal precoEsperado = new BigDecimal("16.25");
+		
+		Assert.assertEquals(precoEsperado, ingresso.getPreco());
+		
+	}
+}
